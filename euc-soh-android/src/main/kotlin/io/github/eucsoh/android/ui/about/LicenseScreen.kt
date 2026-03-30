3fb54e1a9360cdf528ec4a21c5594ee78a4089dc
tritbool <deadubed@protonmail.com>
2026-03-30 14:35:14 +0200

    Preparing for F-droid + cleansing

--- euc-soh-android/src/main/kotlin/io/github/eucsoh/android/ui/about/LicenseScreen.kt
+++ euc-soh-android/src/main/kotlin/io/github/eucsoh/android/ui/about/LicenseScreen.kt
@@ -7,11 +7,9 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
