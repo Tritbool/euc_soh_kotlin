8a0b28ba6d440c11ce1f44544f57e33b97e5aa23
tritbool <deadubed@protonmail.com>
2026-05-04 14:53:06 +0200

    There was major issues with files in archives. Forcing SAF mode made file names harder to get. Files still have a disgusting naming so far. Probably won't change that soon

--- euc-soh-android/build.gradle.kts
+++ euc-soh-android/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "io.github.eucsoh.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10001
+        versionCode = 10002
         versionName = "2.00"
     }
 
