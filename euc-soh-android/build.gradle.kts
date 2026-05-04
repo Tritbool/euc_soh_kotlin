e81690778c8700ea993a3a91072083ac974772f5
tritbool <deadubed@protonmail.com>
2026-05-04 13:28:24 +0200

    First divergence with playstore version (F-droid version has full features whilst Play store versions has restricted files access and features)

--- euc-soh-android/build.gradle.kts
+++ euc-soh-android/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "io.github.eucsoh.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.5"
+        versionCode = 18
+        versionName = "1.51"
     }
 
     buildTypes {
